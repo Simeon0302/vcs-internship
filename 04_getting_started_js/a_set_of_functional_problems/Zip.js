91d11cd35cf17c2276e27969ea80ea8638de0aa1
Simeon_32 <simeonvutov2005@gmail.com>
2022-07-12 13:06:34 +0300

    Adding solved Zip exercise

---
+++ 04_getting_started_js/a_set_of_functional_problems/Zip.js
@@ -0,0 +1,17 @@
+const zip = function(){
+    const resultList = [];
+
+    for(let i = 0; i < arguments.length; i++) {
+        resultList[i] = [];
+        for(let j = 0; j < arguments.length; j++) {
+            resultList[i].push(arguments[i][j]);
+        }
+    }
+    return resultList;
+}
+
+console.log(zip([1, 2, 3], [4, 5, 6]));
+// [ [1, 4], [2, 4], [3, 6] ]
+
+console.log(zip([1, 2, 3], [4, 5, 6], [7, 8, 9]));
+// [ [1, 4, 7], [2, 4, 8], [3, 6, 9] ]
