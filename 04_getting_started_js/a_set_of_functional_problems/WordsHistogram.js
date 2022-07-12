9e337628fa77371900f75be869b61fb4c2ab3cb7
Simeon_32 <simeonvutov2005@gmail.com>
2022-07-12 10:59:09 +0300

    Adding solved WordsHistogram exercise

---
+++ 04_getting_started_js/a_set_of_functional_problems/WordsHistogram.js
@@ -0,0 +1,27 @@
+const wordsHistogram = function (string) {
+    const arr = string.toLowerCase().replace(/[^a-z\s]/g, "").split(" ");
+    const resultObj = {};
+
+    arr.forEach(function (item) {
+        if (!resultObj.hasOwnProperty(item)) {
+            resultObj[item] = 0;
+        }
+        resultObj[item] += 1;
+    });
+    
+    console.log(resultObj);
+};
+
+var str = "A function is a function with a very functional function!";
+wordsHistogram(str);
+
+//Result must be:
+
+// {
+//     "a" : 3,
+//     "function" : 3,
+//     "is" : 1,
+//     "with" : 1,
+//     "very" : 1,
+//     "functional" : 1
+// }
