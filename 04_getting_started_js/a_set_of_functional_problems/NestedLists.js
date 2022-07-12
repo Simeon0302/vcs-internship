e00fed5357373d44ad8ab7db8471344641917c1b
Simeon_32 <simeonvutov2005@gmail.com>
2022-07-12 12:51:15 +0300

    Adding solved Nested Lists exercise

---
+++ 04_getting_started_js/a_set_of_functional_problems/NestedLists.js
@@ -0,0 +1,43 @@
+const ol = function(items){
+    let insideHTML = "";
+
+    items.forEach(itemObj => {
+        if(itemObj.hasOwnProperty("label")){
+            insideHTML += `<li>${itemObj.label}</li>`
+        }
+        if(itemObj.hasOwnProperty("children")){
+            insideHTML += ol(itemObj["children"])
+        }
+    })
+    
+    return `<ol>${insideHTML}</ol>`
+}
+
+const ul = function (items) {
+    let insideHTML = "";
+
+    items.forEach(itemObj => {
+        if (itemObj.hasOwnProperty("label")) {
+            insideHTML += `<li>${itemObj.label}</li>`
+        }
+        if (itemObj.hasOwnProperty("children")) {
+            insideHTML += ol(itemObj["children"])
+        }
+    })
+
+    return `<ul>${insideHTML}</ul>`
+}
+
+var items = [{ "label": "Item 1" },
+{
+    "label": "Item 2", "children": [
+        {
+            "label": "Level 2 of Item 2"
+        },
+        {
+            "label": "Another level 2"
+        }
+    ]
+}];
+
+console.log(ol(items));
