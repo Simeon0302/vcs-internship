a9b14c7111534a662a01035270bf7d7dc901c9fc
Simeon_32 <simeonvutov2005@gmail.com>
2022-07-13 10:40:34 +0300

    Fix all linter errors

--- 04_getting_started_js/oop_problems/ArrayPrototypes.js
+++ 04_getting_started_js/oop_problems/ArrayPrototypes.js
@@ -1,31 +1,30 @@
+/* eslint-disable no-extend-native */
 Array.prototype.first = function() {
     if (this.length < 0) {
-        throw new Error("Array is empty");
+        throw new Error('Array is empty');
     }
     return this[0];
-}
+};
 
 Array.prototype.range = function(from, to) {
     const resultList = [];
     for (let i = from; i <= to; i++) {
-        resultList.push(i);        
+        resultList.push(i);
     }
 
     return resultList;
-}
+};
 
 Array.prototype.sum = function() {
     return this.reduce((a, b) => a + b);
-}
+};
 
 Array.prototype.average = function() {
     return this.sum() / this.length;
-}
+};
 
-
-//Tests
-
-console.log([1, 2, 3, 4].first())
-console.log([].range(1, 10))
-console.log([1, 2, 3, 4, 5].sum())
-console.log([1, 2, 3, 4, 5].average())
+// Tests
+console.log([1, 2, 3, 4].first());
+console.log([].range(1, 10));
+console.log([1, 2, 3, 4, 5].sum());
+console.log([1, 2, 3, 4, 5].average());
