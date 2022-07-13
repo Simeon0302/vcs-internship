c817b86838a866e2dbf5f5b967fdeefc070fc965
Simeon_32 <simeonvutov2005@gmail.com>
2022-07-13 10:08:12 +0300

    Fix all linter errors

--- 04_getting_started_js/a_set_of_functional_problems/NestedLists.js
+++ 04_getting_started_js/a_set_of_functional_problems/NestedLists.js
@@ -1,43 +1,59 @@
-const ol = function(items){
-    let insideHTML = "";
+const ol = function(items) {
+    let insideHTML = '';
 
-    items.forEach(itemObj => {
-        if(itemObj.hasOwnProperty("label")){
-            insideHTML += `<li>${itemObj.label}</li>`
+    items.forEach((itemObj) => {
+        if (itemObj.hasOwnProperty('label')) {
+            insideHTML += `<li>${itemObj.label}</li>`;
         }
-        if(itemObj.hasOwnProperty("children")){
-            insideHTML += ol(itemObj["children"])
+        if (itemObj.hasOwnProperty('children')) {
+            insideHTML += ol(itemObj['children']);
         }
-    })
-    
-    return `<ol>${insideHTML}</ol>`
-}
+    });
 
-const ul = function (items) {
-    let insideHTML = "";
+    return `<ol>${insideHTML}</ol>`;
+};
 
-    items.forEach(itemObj => {
-        if (itemObj.hasOwnProperty("label")) {
-            insideHTML += `<li>${itemObj.label}</li>`
-        }
-        if (itemObj.hasOwnProperty("children")) {
-            insideHTML += ol(itemObj["children"])
-        }
-    })
+const ul = function(items) {
+    let insideHTML = '';
 
-    return `<ul>${insideHTML}</ul>`
-}
-
-var items = [{ "label": "Item 1" },
-{
-    "label": "Item 2", "children": [
-        {
-            "label": "Level 2 of Item 2"
-        },
-        {
-            "label": "Another level 2"
+    items.forEach((itemObj) => {
+        if (itemObj.hasOwnProperty('label')) {
+            insideHTML += `<li>${itemObj.label}</li>`;
         }
-    ]
-}];
+        if (itemObj.hasOwnProperty('children')) {
+            insideHTML += ol(itemObj['children']);
+        }
+    });
+
+    return `<ul>${insideHTML}</ul>`;
+};
+
+// Tests
+
+const items = [{'label': 'Item 1'},
+    {
+        'label': 'Item 2', 'children': [
+            {
+                'label': 'Level 2 of Item 2',
+            },
+            {
+                'label': 'Another level 2',
+            },
+        ],
+    }];
 
 console.log(ol(items));
+
+const items2 = [{'label': 'Item 1'},
+    {
+        'label': 'Item 2', 'children': [
+            {
+                'label': 'Level 2 of Item 2',
+            },
+            {
+                'label': 'Another level 2',
+            },
+        ],
+    }];
+
+console.log(ul(items2));
