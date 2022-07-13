ce7b017695de696ba5c6cd142f1b94bb46959913
Simeon_32 <simeonvutov2005@gmail.com>
2022-07-13 09:53:31 +0300

    Fix all linter errors

--- 04_getting_started_js/a_set_of_functional_problems/CharsHistogram.js
+++ 04_getting_started_js/a_set_of_functional_problems/CharsHistogram.js
@@ -1,22 +1,22 @@
-const charsHistogram = function (string){
-    string = string.toLowerCase().replace(/[^a-z]/g, "");
+const charsHistogram = function(string) {
+    string = string.toLowerCase().replace(/[^a-z]/g, '');
     const arr = [...string];
     const resultObj = {};
-    
-    arr.forEach(item => {
-        if(!resultObj.hasOwnProperty(item)){
+
+    arr.forEach((item) => {
+        if (!resultObj.hasOwnProperty(item)) {
             resultObj[item] = 0;
         }
         resultObj[item] += 1;
-    })
+    });
 
     return resultObj;
-}
+};
 
-var str = "Count the characters in this very profound sentence";
+const str = 'Count the characters in this very profound sentence';
 console.log(charsHistogram(str));
 
-//Result must be
+// Result must be
 
 // { c: 4,
 //   o: 3,
