25efd678c1beb6f7c7d74ff2ef9bbd92469d2c7c
Simeon_32 <simeonvutov2005@gmail.com>
2022-07-13 10:39:05 +0300

    Fix all linter errors

--- 04_getting_started_js/oop_problems/StringPrototype.js
+++ 04_getting_started_js/oop_problems/StringPrototype.js
@@ -1,29 +1,30 @@
+/* eslint-disable no-extend-native */
 String.prototype.capitalize = function() {
     return this.toUpperCase();
-}
+};
 
 String.prototype.dasharize = function() {
-    return this.replace(/[_]/g, "-");
-}
+    return this.replace(/[_]/g, '-');
+};
 
-String.prototype.times = function(amount){
-    let resultString = "";
+String.prototype.times = function(amount) {
+    let resultString = '';
 
-    while(amount > 0){
-        resultString += this + " "
+    while (amount > 0) {
+        resultString += this + ' ';
         amount--;
     }
 
     return resultString.trimEnd();
-}
+};
 
 String.prototype.blank = function() {
-    return this.trim().length === 0
-}
+    return this.trim().length === 0;
+};
 
-console.log("javascript".capitalize())
-console.log("border_bottom_width".dasharize())
-console.log("bobi".times(5))
-console.log("  ".blank()) // true
-console.log("".blank()) // true
-console.log(" a".blank()) // false
+console.log('javascript'.capitalize());
+console.log('border_bottom_width'.dasharize());
+console.log('bobi'.times(5));
+console.log('  '.blank()); // true
+console.log(''.blank()); // true
+console.log(' a'.blank()); // false
