bfac7f203491f394fbaed11a79da8ca4e64060bf
Simeon_32 <simeonvutov2005@gmail.com>
2022-07-13 10:06:37 +0300

    Fix all linter errors

--- 04_getting_started_js/a_set_of_functional_problems/OrderedAndUnordered.js
+++ 04_getting_started_js/a_set_of_functional_problems/OrderedAndUnordered.js
@@ -1,19 +1,24 @@
-const ol = function(items){
-    let insideHTML = "";
-    items.forEach(itemObj => {
-        insideHTML = insideHTML.concat(`<li>${itemObj.label}</li>`)
-    })
-    return `<ol>${insideHTML}</ol>`
-}
+const ol = function(items) {
+    let insideHTML = '';
+    items.forEach((itemObj) => {
+        insideHTML = insideHTML.concat(`<li>${itemObj.label}</li>`);
+    });
+    return `<ol>${insideHTML}</ol>`;
+};
 
-const ul = function(items){
-    let insideHTML = "";
-    items.forEach(itemObj => {
-        insideHTML = insideHTML.concat(`<li>${itemObj.label}</li>`)
-    })
-    return `<ul>${insideHTML}</ul>`
-}
+const ul = function(items) {
+    let insideHTML = '';
+    items.forEach((itemObj) => {
+        insideHTML = insideHTML.concat(`<li>${itemObj.label}</li>`);
+    });
+    return `<ul>${insideHTML}</ul>`;
+};
 
-var items = [{ "label": "Item 1" }, { "label": "Item 2" }]
-var htmlOl = ol(items);
+// Tests
+const items = [{'label': 'Item 1'}, {'label': 'Item 2'}];
+const htmlOl = ol(items);
 console.log(htmlOl);
+
+const items2 = [{'label': 'Item 1'}, {'label': 'Item 2'}];
+const htmlUl = ul(items2);
+console.log(htmlUl);
