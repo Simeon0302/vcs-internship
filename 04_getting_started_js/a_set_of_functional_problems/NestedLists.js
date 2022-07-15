70a97c77dbf6c24cb92d231749882d756c6ee67f
Simeon_32 <simeonvutov2005@gmail.com>
2022-07-15 14:29:36 +0300

    Configure the tests, so now they can check for expected value and print if everything is alright. Fix problems with the ol and ul functions.

--- 04_getting_started_js/a_set_of_functional_problems/NestedLists.js
+++ 04_getting_started_js/a_set_of_functional_problems/NestedLists.js
@@ -3,10 +3,12 @@ const ol = function(items) {
 
     items.forEach((itemObj) => {
         if (itemObj.hasOwnProperty('label')) {
-            insideHTML += `<li>${itemObj.label}</li>`;
-        }
-        if (itemObj.hasOwnProperty('children')) {
-            insideHTML += ol(itemObj['children']);
+            if (itemObj.hasOwnProperty('children')) {
+                insideHTML +=
+                    `<li>${itemObj.label}${ol(itemObj['children'])}</li>`;
+            } else {
+                insideHTML += `<li>${itemObj.label}</li>`;
+            }
         }
     });
 
@@ -18,10 +20,12 @@ const ul = function(items) {
 
     items.forEach((itemObj) => {
         if (itemObj.hasOwnProperty('label')) {
-            insideHTML += `<li>${itemObj.label}</li>`;
-        }
-        if (itemObj.hasOwnProperty('children')) {
-            insideHTML += ul(itemObj['children']);
+            if (itemObj.hasOwnProperty('children')) {
+                insideHTML +=
+                    `<li>${itemObj.label}${ul(itemObj['children'])}</li>`;
+            } else {
+                insideHTML += `<li>${itemObj.label}</li>`;
+            }
         }
     });
 
@@ -29,8 +33,12 @@ const ul = function(items) {
 };
 
 // Tests
+const assert = require('assert');
+const expectedResultOl = '<ol><li>Item 1</li><li>Item 2<ol><li>Level 2 of Item 2</li><li>Another level 2</li></ol></li></ol>';
+const expectedResultUl = '<ul><li>Item 1</li><li>Item 2<ul><li>Level 2 of Item 2</li><li>Another level 2</li></ul></li></ul>';
 
-const items = [{'label': 'Item 1'},
+
+const testItemsOl = [{'label': 'Item 1'},
     {
         'label': 'Item 2', 'children': [
             {
@@ -42,9 +50,16 @@ const items = [{'label': 'Item 1'},
         ],
     }];
 
-console.log(ol(items));
 
-const items2 = [{'label': 'Item 1'},
+try {
+    assert.strictEqual(ol(testItemsOl), expectedResultOl);
+    console.log('Ol function test passed');
+} catch {
+    console.log('Ol function test failed');
+}
+
+
+const testItemsUl = [{'label': 'Item 1'},
     {
         'label': 'Item 2', 'children': [
             {
@@ -56,4 +71,10 @@ const items2 = [{'label': 'Item 1'},
         ],
     }];
 
-console.log(ul(items2));
+
+try {
+    assert.strictEqual(ul(testItemsUl), expectedResultUl);
+    console.log('Ul function test passed');
+} catch {
+    console.log('Ul function test failed');
+}
